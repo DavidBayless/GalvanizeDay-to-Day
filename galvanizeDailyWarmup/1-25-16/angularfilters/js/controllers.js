77fa6b6249dc7eb2073519b122c8504f086cb4d4
David Bayless <David.C.Bayless15@gmail.com>
2016-01-25 18:59:23 -0700

    added active labeling on an SPA using bootstrap

--- galvanizeDailyWarmup/1-25-16/angularfilters/js/controllers.js
+++ galvanizeDailyWarmup/1-25-16/angularfilters/js/controllers.js
@@ -3,4 +3,50 @@ app.controller('mainController', ['$scope', mainController]);
 function mainController($scope) {
   var vm = this;
   vm.pageTitle = "Angular Custom Filters";
+  // $scope.active = 'home';
+}
+
+app.controller('secondController', ['$scope', secondController]);
+
+function secondController($scope) {
+  var vm = this;
+  vm.pageTitle = "THE USS CHESHIRE";
+}
+
+app.controller('projectsController', ['$scope', projectsController]);
+
+function projectsController($scope) {
+  var vm = this;
+  vm.pageTitle = "Stuff about stuff I've done";
+  // vm.active = 'projects';
+}
+
+app.controller('bioController', ['$scope', bioController]);
+
+function bioController($scope) {
+  var vm = this;
+  vm.pageTitle = "Stuff about me";
+  // vm.active = 'bio';
+}
+
+app.controller('resumeController', ['$scope', resumeController]);
+
+function resumeController($scope) {
+  var vm = this;
+  vm.pageTitle = "Stuff about why you should hire me";
+  // vm.active = 'resume'
+}
+
+app.controller('indexController', ['$scope', '$location', indexController]);
+
+function indexController($scope, $location) {
+  var vm = this;
+  console.log($location.$$path);
+  vm.path = $location.$$path;
+  vm.changePath = function() {
+    vm.path = $location.$$path;
+  }
+  vm.pageTitle = "Stuff about why you should hire me";
+
+  // vm.active = 'resume'
 }
--- galvanizeDailyWarmup/1-25-16/angularfilters/js/routes.js
+++ galvanizeDailyWarmup/1-25-16/angularfilters/js/routes.js
@@ -8,4 +8,16 @@ app.config(function($routeProvider) {
         templateUrl: 'views/second.html',
         controller: 'secondController as SC'
       })
+      .when('/projects', {
+        templateUrl: 'views/projects.html',
+        controller: 'projectsController as PC'
+      })
+      .when('/resume', {
+        templateUrl: 'views/resume.html',
+        controller: 'resumeController as RC'
+      })
+      .when('/bio', {
+        templateUrl: 'views/bio.html',
+        controller: 'bioController as BC'
+      });
 });
